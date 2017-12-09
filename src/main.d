98d8692af980e1a36a8b5873b1e3bca368d4618f
karanokuri <karanokuri@gmail.com>
2017-12-10 03:25:46 +0900

    不要なコメントを削除

--- src/main.d
+++ src/main.d
@@ -4,30 +4,10 @@ import wave;
 
 int main()
 {
-	//auto wave = new Wave("アルト.wav");
-
-	//wave.timeStretch(2);
-	//wave.pitchShift(2);
-	//wave.write("conv_アルト.wav");
-
-	//wave.semitoneShift(5);
-	//wave.write("alto_to_soprano.wav");
-	//wave.semitoneShift(-5);
-	//wave.write("soprano_to_alto.wav");
-
-	//wave.read("alto.wav");
-	//wave.semitoneShift(-7);
-	//wave.write("alto_to_tenor.wav");
-
-	//if(wave.samplesPerSec == 0 || wave.samplesPerSec != 0)
-	//	return 0;
-
 	int result = 0;
 
 	try
 	{
-		// Application initialization code here.
-
 		Application.enableVisualStyles();
 		Application.run(new ConvForm);
 	}
