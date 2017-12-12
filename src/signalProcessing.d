7685b3781f33db44e3e68b8effd2a96c9e76dc81
karanokuri <karanokuri@gmail.com>
2017-12-13 00:07:15 +0900

    型の宣言を説明的なものに変更

--- src/signalProcessing.d
+++ src/signalProcessing.d
@@ -19,7 +19,7 @@ import std.range;
 auto autoCorrelation(R)(R range, size_t len, size_t lag)
   if (isRandomAccessRange!R && hasLength!R)
 {
-  typeof(range[0]) ret = 0;
+  ElementType!R ret = 0;
   for(int i = 0; i < len; i++)
     ret += range[i] * range[i+lag];
 
@@ -41,7 +41,7 @@ size_t peakOfAutoCorrelation(R)(R range, size_t len, size_t lagMin, size_t lagMa
   if (isRandomAccessRange!R && hasLength!R)
 {
   auto peak = lagMin;
-  typeof(range[0]) max = 0;
+  ElementType!R max = 0;
   for(int lag = lagMin; lag <= lagMax; lag++)
   {
     auto val = range.autoCorrelation(len, lag);
